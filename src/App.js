23de26fedcc2496b45a3ff0b54341ba950f6c17e
Jennifer Lang <jenniferlang1921@gmail.com>
2018-06-07 13:33:51 -0500

    pre-logic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CountryCard from "./components/CountryCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import countries from "./countries.json";
 import './App.css';
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    countries
+  };
+
+  removeCountry= id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const countries = this.state.countries.filter(country => country.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ countries });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Title>Travel Around The World In Twelve Clicks</Title>
+        {this.state.countries.map(country => (
+          <CountryCard
+            removeCountry={this.removeCountry}
+            id={country.id}
+            key={country.id}
+            name={country.name}
+            image={country.url}
+            
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
